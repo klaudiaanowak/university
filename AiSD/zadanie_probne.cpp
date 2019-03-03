7a9787f48ccb7a5daace27af8fac585ec44a5df3
klaudia <klaudia_nowak@op.pl>
2019-03-03 15:04:50 +0100

    final version

--- AiSD/zadanie_probne.cpp
+++ AiSD/zadanie_probne.cpp
@@ -1,24 +1,30 @@
 #include <cstdio>
 
-void swap(int *a, int *b){
-    if (*a > *b){
+void swap(int *a, int *b)
+{
+    if (*a > *b)
+    {
         int temp = *a;
         *a = *b;
         *b = temp;
     }
 }
 
-int main(){
-    int a,b;
-   if(scanf("%d %d", &a, &b) == EOF) {
-	   printf("Error occured while reading form STDIN");
-   }
+int main()
+{
+    int a, b;
+    if (scanf("%d  %d", &a, &b) != 2)
+    {
+        return -1;
+    }
     swap(&a, &b);
-	if (a < 0 || b > 1000){
-		return 1;
-	}
-    while (a<=b){
-        printf("%d\n",a);
+    if (a < 0 || b > 1000)
+    {
+        return -1;
+    }
+    while (a <= b)
+    {
+        printf("%d\n", a);
         a++;
     }
     return 0;
