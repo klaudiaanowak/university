4cdc72ae755343040c689025654bb0267e3597d8
klaudia <klaudia_nowak@op.pl>
2019-03-03 23:12:14 +0100

    pierwsze zadanie na themis

---
+++ AiSD/lista_powitalna.cpp
@@ -0,0 +1,41 @@
+// Klaudia Nowak 
+//297936 
+//SDU
+
+#include <cstdio>
+
+void swap(int *a, int *b)
+{
+    if (*a > *b)
+    {
+        int temp = *a;
+        *a = *b;
+        *b = temp;
+    }
+}
+
+int main()
+{
+    int a, b;
+    if (scanf("%d  %d", &a, &b) != 2)
+    {
+        return -1;
+    }
+    swap(&a, &b);
+   
+    if (a < 0 || b > 1000000000 || b - a >= 100000)
+    {
+        return -1;
+    }
+    a = a - (a % 2018);
+    if (a == 0) {
+        a+=2018;
+    }
+    while (a <= b)
+    {
+        printf("%d ", a);
+        a = a + 2018;
+    }
+
+    return 0;
+}
