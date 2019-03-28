b098c7d725c16eff58ec3c97a90bc5aad8118334
klaudia <klaudia_nowak@op.pl>
2019-03-28 15:51:08 +0100

    first good version of zadanieB

---
+++ AiSD/klnowak_zadanieB.cpp
@@ -0,0 +1,79 @@
+#include <cstdio>
+#include <iostream>
+
+void zamien(long long int *a, long long int *b)
+{
+    long long int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void przywroc_porzadek(long long int M[][2], int n)
+{
+    bool flag = true;
+    int i = 0;
+    while (flag && 2*i + 1< n)
+    {
+        long long int e = M[2 * i][1] * M[2 * i][0];
+        long long int l = M[2 * i + 1][0] * M[2 * i + 1][1];
+        long long int r = M[2 * i + 2][0] * M[2 * i + 2][1];
+        if ( e < l || e < r )
+        {
+
+            if (l > r)
+            {
+                zamien(&M[2 * i][1], &M[2 * i + 1][1]);
+                zamien(&M[2 * i][0], &M[2 * i + 1][0]);
+
+                i += 1;
+            }
+            else
+            {
+                zamien(&M[2 * i][1], &M[2 * i + 2][1]);
+                zamien(&M[2 * i][0], &M[2 * i + 2][0]);
+
+                i += 1;
+            }
+        }
+        else
+        {
+            flag = false;
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    long long int k;
+
+    if ((scanf("%d %lld", &n, &k) != 2) || n < 1 || n > 1000000 || k < 1 || k > 2000000)
+    {
+        return -1;
+    }
+    long long int M[n][2];
+
+    int i = 0;
+    while (i < n)
+    {
+        M[i][0] = n - i;
+        M[i][1] = n - i;
+        i++;
+    }
+    int j = 0;
+    long long int max = 0;
+
+    while (j < k && M[0][1] >= 0)
+    {
+
+        przywroc_porzadek(M, n);
+
+        if (max != (M[0][0] * M[0][1]) && M[0][1] != 0)
+        {
+            max = (M[0][0] * M[0][1]);
+            printf("%lld\n", max);
+            j++;
+        }
+        M[0][1] -= 1;
+    }
+}
--- AiSD/klnowak_zadanie_A.cpp
+++ AiSD/klnowak_zadanie_A.cpp
@@ -1,6 +1,6 @@
 #include <cstdio>
 
-int min(int a, int b)
+long long int min(long long int a, long long int b)
 {
     if (a < b)
     {
@@ -12,7 +12,7 @@ int min(int a, int b)
     }
 }
 
-int max(int a, int b)
+long long int max(long long int a, long long int b)
 {
     if (a > b)
     {
@@ -30,48 +30,62 @@ int main()
     {
         return -1;
     }
-    int K[n];
+    long long int K[n];
     int i = 0;
-    int sum = 0;
+    long long int sum = 0;
 
     while (i < n)
     {
-        if ((scanf("%d", &K[i]) != 1) || K[i] < 0 || K[i] > 1000000000)
+        if ((scanf("%lld", &K[i]) != 1) || K[i] < 0 || K[i] > 1000000000)
         {
             return -1;
         }
         sum += K[i];
         i++;
     }
-    
-    bool flag_f = true;
-    bool flag_s = true;
+
+    bool flag_f = false;
+    bool flag_s = false;
+    bool move_f = false;
+    bool move_s = false;
     int begin_f = 0;
     int begin_s = 1;
-    int sum_f = K[begin_f];
-    int sum_s = sum - sum_f;
-    int max_dist = min(sum_f, sum_s);
+    long long int sum_f = K[begin_f];
+    long long int sum_s = sum - sum_f;
+    long long int max_dist = min(sum_f, sum_s);
     int index_f = begin_f;
     int index_s = begin_s;
 
-    while (index_f != begin_f || flag_f)
+    while (!(flag_f && flag_s))
     {
         if (sum_f < sum_s)
         {
-            flag_s = false;
+            move_s = true;
             sum_s -= K[index_s];
             sum_f += K[index_s];
             index_s = (index_s + 1) % n;
         }
         else
         {
-            flag_f = false;
+            move_f = true;
             sum_f -= K[index_f];
             sum_s += K[index_f];
             index_f = (index_f + 1) % n;
         }
 
         max_dist = max(max_dist, min(sum_f, sum_s));
+        if (max_dist == sum / 2)
+        {
+            break;
+        }
+        if (index_f == begin_f && move_f)
+        {
+            flag_f = true;
+        }
+        if (index_s == begin_s && move_s)
+        {
+            flag_s = true;
+        }
     }
-    printf("%d\n", max_dist);
+    printf("%lld\n", max_dist);
 }
