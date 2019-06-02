1a26790a2a577ef9711d4a8460fddcc0bae8e8b1
klaudiaanowak <klaudia_nowak@op.pl>
2019-06-02 20:19:11 +0200

    open function

--- projekt-bazy-danych/init.py
+++ projekt-bazy-danych/init.py
@@ -1,33 +1,50 @@
-
 import psycopg2
+import json
+
 
 def run_sql_file(filename, connection):
-    '''
-    The function takes a filename and a connection as input
-    and will run the SQL query on the given connection  
-    '''
-    
+
     file = open(filename, 'r')
     sql = " ".join(file.readlines())
     cursor = connection.cursor()
-    cursor.execute(sql)    
+    cursor.execute(sql)
     connection.commit()
-    
-    print ("Time elapsed to run the query:")
 
-conn = psycopg2.connect(database = "student", user = "postgres")
-print ("Opened database successfully")
+    print("Time elapsed to run the query:")
 
-# cur = conn.cursor()
-# cur.execute("""
-#         CREATE TABLE vendors (
-#             vendor_id SERIAL PRIMARY KEY,
-#             vendor_name VARCHAR(255) NOT NULL
-#         )
-#         """)
-run_sql_file("database.sql", conn)    
 
-print ("Table created successfully")
+def fun_open(database, user, password):
+    try:
+        conn = psycopg2.connect(database=database, user=user, password=password)
+        print("Opened database successfully")
+        run_sql_file("database.sql", conn)
+        print(json.dumps({"status": "OK"}))
+        print("Table created successfully")
+    except:
+        print(json.dumps({"status": "ERROR"}))
 
-conn.commit()
-conn.close()
+    while (True):
+        new_input = input()
+        json_input = json.loads(input)
+
+
+    conn.commit()
+    conn.close()
+
+
+def main():
+
+    #json_input = input()
+    json_input = '{ "open": { "database": "student", "login": "init", "password": "qwerty"}}'
+    json_loaded = json.loads(json_input)
+
+
+    if(list(json_loaded.keys())[0] == 'open'):
+        open_json = json.loads(json.dumps(json_loaded["open"]))
+        fun_open(open_json["database"], open_json["login"], open_json["password"])
+
+    # conn = psycopg2.connect(database = "student", user = "postgres")
+
+
+if __name__ == "__main__":
+    main()
